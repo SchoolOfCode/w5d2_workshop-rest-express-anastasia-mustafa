ed05c801d0e82ce75461d811c85597d118379e16
Mustafa <Mustafa.mohamad239@gmail.com>
2022-05-25 17:37:48 +0100

    Fixed the errors, Can create tables and populate

--- db/scripts/dropBooksTable.js
+++ db/scripts/dropBooksTable.js
@@ -1,10 +1,10 @@
-// import {query} from "../index.js";
+import {query} from "../index.js";
 
-// const sqlString = 'DROP TABLE IF EXISTS books;';
+const sqlString = 'DROP TABLE IF EXISTS books;';
 
-// async function createBooksTable(){
-//     const res = await query(sqlString);
-//     console.log(res.command, "Dropped books table")
-// }
+async function createBooksTable(){
+    const res = await query(sqlString);
+    console.log(res.command, "Dropped books table")
+}
 
-// createBooksTable();
+createBooksTable();
