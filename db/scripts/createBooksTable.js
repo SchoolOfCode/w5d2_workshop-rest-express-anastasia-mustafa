a620123604420535fd8169a05439493ca3647ac1
Mustafa <Mustafa.mohamad239@gmail.com>
2022-05-25 16:49:51 +0100

    Debugging error triggerUncaughtException

--- db/scripts/createBooksTable.js
+++ db/scripts/createBooksTable.js
@@ -1,7 +1,6 @@
-import {query} from '../index.js';
+import { query } from '../index.js';
 
-const sqlString = `CREATE TABLE IF NOT EXISTS books (book_id INT GENERATED ALWAYS AS IDENTITY,
-     author_id INT, title TEXT);`;
+const sqlString = `CREATE TABLE IF NOT EXISTS books (book_id INT GENERATED ALWAYS AS IDENTITY, author_id INT, title TEXT);`;
      //add publish dates
 async function createBooksTable() {
     const res = await query(sqlString);
--- db/scripts/populateBooksTable.js
+++ db/scripts/populateBooksTable.js
@@ -4,8 +4,7 @@ import {books} from '../../libs/data.js';
 async function populateBooksTable() {
     for (let i = 0; 1 < books.length; i++) {
         const res = await query (
-            `INSERT INTO books (author_id, title) VALUES ($1, $2) RETURNING *`,
-            [books[i].author_id, books[i].title]
+            `INSERT INTO books (author_id, title) VALUES ($1, $2) RETURNING *`,[books[i].author_id, books[i].title]
         );
         console.log(res.rows[0].title, 'inserted');
     }
